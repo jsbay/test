5b3fea583f8d35969a2e5bc8975864e5553cb21a
BayMax.MacBook <13140111181@163.com>
2020-11-18 11:21:42 +0800

    再优化

--- egg/app/extend/application.js
+++ egg/app/extend/application.js
@@ -6,15 +6,7 @@
  * @Description  :
  */
 'use strict';
-const path = require('path');
-
 const sql = require('mssql');
-const mssqlConfig = require(path.join(process.cwd(), '/config/config.mssql'));
-const pool = new sql.ConnectionPool(mssqlConfig);
-
-pool.on('error', err => {
-  err && console.log(err);
-});
 
 const DB = Symbol('Application#db');
 
@@ -25,21 +17,28 @@ module.exports = {
     }
     this[DB] = {
       config: this.config.mssql,
+      pool: new sql.ConnectionPool(this.config.mssql),
       async execProcedure(procedureName, params) {
         // this 就是 app 对象，在其中可以调用 app 上的其他方法，或访问属性
-        console.log('execProcedure', this);
-        await pool.connect();
-        const ps = new sql.PreparedStatement(pool);
-        if (params && Object.getOwnPropertyNames(params).length > 0) {
-          for (const [ key, value ] of Object.entries(params)) {
-            typeof value === 'number' ? ps.input(key, sql.Int, value) : ps.input(key, sql.NVarChar, value);
+        console.log(this);
+        try {
+          await this.pool.connect();
+          console.log(this.pool);
+          const ps = new sql.PreparedStatement(this.pool);
+          if (params && Object.getOwnPropertyNames(params).length > 0) {
+            for (const [ key, value ] of Object.entries(params)) {
+              typeof value === 'number' ? ps.input(key, sql.Int, value) : ps.input(key, sql.NVarChar, value);
+            }
           }
+          await ps.prepare(procedureName);
+          const result = await ps.execute(params);
+          await ps.unprepare();
+          console.log(result);
+          return result;
+        } catch (error) {
+          console.log(error);
         }
-        await ps.prepare(procedureName);
-        const result = await ps.execute(params);
-        await ps.unprepare();
-        // console.log(result);
-        return result;
+
       },
     };
     return this[DB];
